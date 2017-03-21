d800297c39bf74062dcbf99a67e983a6827d7d79
Aaron Zemetres <azemetre@gmail.com>
2017-03-21 00:40:50 -0400

    test: tests the mock api with fake data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Fake DB
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@example.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@example.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@example.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
